693bdd9357545dec794ef13e645e0381cf87b15b
John Doe <johndoe@example.com>
2022-12-22 16:37:41 +0700

    controllers v1.000000000000001

---
+++ src/controller/article.controller.js
@@ -0,0 +1,51 @@
+const db = require('../models');
+const jwt = require('jsonwebtoken');
+
+const { CustomError, TypeError } = require('../models/customError.model');
+const Article = db.articles;
+
+
+class ArticleController {
+    async getArticles(req, res) {
+        const articles = await Article.findAll();
+        res.json(articles);
+    }
+
+    async createArticle(req, res) {
+        const { name, content, markId, employeePositionId, date, active, sectionId} = req.body;
+
+        const articleBody = {
+            name, content, markId, employeePositionId, date, active, sectionId
+        }
+
+        await Article.create(articleBody);
+        res.json({success: true});
+    }
+
+    async updateArticle(req, res) {
+        const { id, name, content, markId, employeePositionId, date, active, sectionId} = req.body;
+
+        const foundArticle = Article.findOne({
+            where: {
+                id,
+            }
+        });
+        if (!foundArticle) {
+            throw new CustomError(404, TypeError.NOT_FOUND);
+        } 
+
+        const article = { name, content, markId, employeePositionId, date, active, sectionId };
+        await Article.update(article, { 
+            where: { 
+                id 
+            }
+        });
+        await res.json({ success: true });
+    }
+
+    async deleteArticle(req, res) {
+        /* Удаление active: false */
+    }
+}
+
+module.exports = new ArticleController();
---
+++ src/controller/employeePosition.controller.js
@@ -0,0 +1,16 @@
+const db = require('../models');
+const jwt = require('jsonwebtoken');
+
+const { CustomError, TypeError } = require('../models/customError.model');
+const EmployeePosition = db.employeePositions;
+
+
+class EmployeePositionController { 
+    async getEmployeePositions(req, res) {
+        const employees = await axios.get(`http://${process.env.API_1C_USER}:${process.env.API_1C_PASSWORD}@192.168.240.196/zup_pay/hs/Exch_LP/ListEmployees`);
+
+        res.json(employees)
+    }
+}
+
+module.exports = new EmployeePositionController();
