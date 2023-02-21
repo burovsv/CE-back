6bc4936b72b4b1c0c92813511241bf793d669550
John Doe <johndoe@example.com>
2023-02-21 16:20:21 +0700

    add employeePositions

--- src/controller/employeePosition.controller.js
+++ src/controller/employeePosition.controller.js
@@ -1,15 +1,26 @@
 const db = require('../models');
 const jwt = require('jsonwebtoken');
+const { default: axios } = require('axios');
+
 
 const { CustomError, TypeError } = require('../models/customError.model');
 const EmployeePosition = db.employeePositions;
 
 
-class EmployeePositionController { 
-    async getEmployeePositions(req, res) {
-        const employees = await axios.get(`http://${process.env.API_1C_USER}:${process.env.API_1C_PASSWORD}@192.168.240.196/zup_pay/hs/Exch_LP/ListEmployees`);
+class EmployeePositionController {
 
-        res.json(employees)
+    async getEmployeePositions(req, res) {
+        const dataFrom1C = await axios
+            .get(`http://${process.env.API_1C_USER}:${process.env.API_1C_PASSWORD}@192.168.240.196/zup_pay/hs/Exch_LP/ListPost`, {})
+            .then((res) => {
+                return res.data;
+            })
+            .catch((res) => {
+                console.log('error', res.response.data);
+                return [];
+            })
+
+        res.json(dataFrom1C)
     }
 
 }
