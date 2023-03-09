83d75b798a5f0dbbd3ed519241e2171b104c84c5
John Doe <johndoe@example.com>
2023-03-10 00:56:00 +0700

    changed articleFileUpload controller

--- src/controller/articleFileUpload.controller.js
+++ src/controller/articleFileUpload.controller.js
@@ -3,17 +3,30 @@ const { CustomError, TypeError } = require('../models/customError.model');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const moment = require('moment');
+const path = require('path')
+const mime = require("mime");
 
 const ArticleFile = db.articleFiles;
 
 class ArticleFileUploadController {
     async uploadArticleFile(req, res) {
-        // файл articleId
-        // Формируем уникальное имя по дате
-        let name = moment().format("DD-MM-YY_HH-mm-ss");
         
-        console.log(req.body);
-        console.log(req);
+        try {
+            // файл articleId
+            // Формируем уникальное имя по дате
+            let name = moment().format("DD-MM-YY_HH-mm-ss");
+            let file = req.files.file;
+            
+            const type = file.name.split('.').pop();
+            const fullFileName = `${name}.${type}`;
+            let filePath = `./public/article/images/${fullFileName}`;
+            file.mv(filePath)
+
+            res.json(fullFileName)
+            
+        } catch (error) {
+            return res.status(500).json({message: 'Upload error'})
+        }
 
 
     }
